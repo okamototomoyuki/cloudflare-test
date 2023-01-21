5bf17ed0c7cb76e70167d5d5e3b10b1511f3b699
test1234 <test@test1234.com>
2023-01-21 16:54:47 +0900

    a

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,15 +2,21 @@ import type { PageServerLoad } from './$types';
 export const load = (async ({ platform }) => {
     if (platform) {
         // const test_value = platform.env.test_kv.idFromName('test_key');
-        const ps = platform?.env?.test1234.prepare('SELECT * from test_table');
-        const data = await ps.first();
-        const obj: { [key: string]: string } = {};
-        // for (let [k, v] of Object.entries(platform?.env)) {
-        //     obj[k] = v.toString();
-        // }
-        obj["test1234"] = data;
-        // obj["test_kv"] = platform?.env?.test_kv?.toString();
-        return obj;
+        try {
+            const ps = platform?.env?.test1234.prepare('SELECT * from test_table');
+            const data = await ps.first();
+            const obj: { [key: string]: string } = {};
+            // for (let [k, v] of Object.entries(platform?.env)) {
+            //     obj[k] = v.toString();
+            // }
+            obj["test1234"] = data;
+            // obj["test_kv"] = platform?.env?.test_kv?.toString();
+            return obj;
+
+        } catch (e) {
+            return { error: e };
+
+        }
         // return { "data": platform.toString(), "data2": platform?.env?.toString(), "data3": platform?.env?.test_kv?.toString() };
         // return {}
     }
