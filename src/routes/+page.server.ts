e6c7371c0311edec4f8a2cb68f03e6ca0cfe5e55
test1234 <test@test1234.com>
2023-01-21 13:14:35 +0900

    tets

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,6 +4,10 @@ export const load = (async ({ platform }) => {
         // const test_value = platform.env.test_kv.idFromName('test_key');
         // const ps = platform.env.NORTHWIND_DB.prepare('SELECT * from users');
         // const data = await ps.first();
+        const obj: { [key: string]: string } = {};
+        for (let [k, v] of Object.entries(platform?.env){
+            obj[k] = v.toString();
+        }
         return platform?.env;
         // return { "data": platform.toString(), "data2": platform?.env?.toString(), "data3": platform?.env?.test_kv?.toString() };
         // return {}
