747aba75a538333807130b4e62235141dda179bb
test1234 <test@test1234.com>
2023-01-21 12:58:40 +0900

    てｓｔ

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,11 @@
 import type { PageServerLoad } from './$types';
 export const load = (async ({ platform }) => {
     if (platform) {
-        const test_value = platform.env.test_kv.idFromName('test_key');
+        // const test_value = platform.env.test_kv.idFromName('test_key');
         // const ps = platform.env.NORTHWIND_DB.prepare('SELECT * from users');
         // const data = await ps.first();
-        return { test_value };
+        // return { test_value };
+        return {}
     }
     return {}
 }) satisfies PageServerLoad;
