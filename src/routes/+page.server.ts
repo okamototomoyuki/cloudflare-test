8bea91a46861da626d277a86863d972400a4c403
test1234 <test@test1234.com>
2023-01-21 16:50:09 +0900

    a

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,13 +4,13 @@ export const load = (async ({ platform }) => {
         // const test_value = platform.env.test_kv.idFromName('test_key');
         const ps = platform?.env?.test1234.prepare('SELECT * from test_table');
         const data = await ps.first();
-        // const obj: { [key: string]: string } = {};
+        const obj: { [key: string]: string } = {};
         // for (let [k, v] of Object.entries(platform?.env)) {
         //     obj[k] = v.toString();
         // }
-        // obj["test1234"] = data;
+        obj["test1234"] = data;
         // obj["test_kv"] = platform?.env?.test_kv?.toString();
-        return data;
+        return obj;
         // return { "data": platform.toString(), "data2": platform?.env?.toString(), "data3": platform?.env?.test_kv?.toString() };
         // return {}
     }
